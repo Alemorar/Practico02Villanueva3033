9c40cab6128ab1ca59ba22b29373f9d6a8dd63a8
Alemorar <infoserv100@gmail.com>
2015-09-08 13:35:17 -0300

    punto 03 en desarrollo

--- src/Punto03/GestionString.java
+++ src/Punto03/GestionString.java
@@ -12,7 +12,42 @@ import java.util.Scanner;
  * @author nahuel
  */
 public class GestionString {
-  
+    
+    public int contarCaracteres(String cadena){
+        int caracteres = 0;
+        caracteres = cadena.length();
+        return caracteres;
+    }
+    
+    public int contarMinusculas(String cadena){
+        int minusculas = 0;
+        for (int i = 0; i < cadena.length(); i++){
+            if (cadena.charAt(i) == cadena.toLowerCase().charAt(i)){
+                minusculas = minusculas + 1;
+            }
+        }
+        return minusculas;
+    }
+    
+    public int contarMayusculas(String cadena){
+        int mayusculas = 0;
+        for (int i = 0; i < cadena.length(); i++){
+            if (cadena.charAt(i) == cadena.toUpperCase().charAt(i)){
+                mayusculas = mayusculas + 1;
+            }
+        }
+        return mayusculas;
+    }
+    
+    public int contarOcurrencias(String cadena, char letra){
+        int ocurrencia = 0;
+        for (int i = 0; i < cadena.length(); i++){
+            if (cadena.charAt(i) == letra){
+                ocurrencia = ocurrencia + 1;
+            }
+        }
+        return ocurrencia;
+    }
     
     public void determinarPalindrome(String cadena){
         boolean band = false;
@@ -30,14 +65,41 @@ public class GestionString {
         }
     }
     
+    public String invertirLetras(String cadena){
+        for (int i = 0; i < cadena.length(); i++){
+            if (cadena.charAt(i) == Character.toUpperCase(cadena.charAt(i))){
+                cadena.charAt(i) = Character.toLowerCase(cadena.charAt(i));
+            }else if (cadena.charAt(i) == cadena.toLowerCase().charAt(i)){
+                cadena.substring(i, i).toUpperCase();
+            }
+        }
+        return cadena;
+    }
+    
     public void controlarMetodo(){
         Scanner scanner = new Scanner(System.in);
         String palindromiar;
         do{
+            System.out.println("Ingrese una opcion");
+            System.out.println("1) Contar letras");
+            System.out.println("2) Contar minusculas");
+            System.out.println("3) Contar mayusculas");
+            System.out.println("4) Contar ocurrencia de letra");
+            System.out.println("5) Determinar palindrome");
+            System.out.println("6) Invertir letras");
+            System.out.println("Salir 'S'");
+            
+            
+                    
             System.out.println("Ingrese palabra. 'S'alir");
             palindromiar = scanner.next();
+            System.out.println(contarCaracteres(palindromiar));
+            System.out.println(contarMinusculas(palindromiar));
+            System.out.println(contarMayusculas(palindromiar));
+
             determinarPalindrome(palindromiar);
+            System.out.println(invertirLetras(palindromiar));
         }while (!"s".equals(palindromiar));
         System.out.println("Saliendo del system.......");
-    }   
+    }
 }
