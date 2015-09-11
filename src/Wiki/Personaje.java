354ecd424ee67e958ad34db62157992cde86c1dd
Alemorar <infoserv100@gmail.com>
2015-09-11 01:31:42 -0300

    wiki

---
+++ src/Wiki/Personaje.java
@@ -0,0 +1,25 @@
+package Wiki;
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author alejandro
+ */
+public class Personaje {
+    private String nombre;
+    private Vocal unaVocal;
+    private ArrayList<ObjetoConVocal> obj;
+
+    public Personaje() {
+        ArrayList<ObjetoConVocal> unObj;
+    }
+    
+    
+}
---
+++ src/Wiki/Vocal.java
@@ -0,0 +1,19 @@
+package Wiki;
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author alejandro
+ */
+public class Vocal {
+    private char vocal;
+     
+    
+}
